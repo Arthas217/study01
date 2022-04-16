bb83ee5f044aea8cc50bd1ba0218d57609801adb
Arthas217 <442854399@qq.com>
2022-04-16 13:13:00 +0800

    schedule go on

--- src/main/java/com/example/study01/schedule/SpringbootScheduleTask.java
+++ src/main/java/com/example/study01/schedule/SpringbootScheduleTask.java
@@ -1,7 +1,6 @@
 package com.example.study01.schedule;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
