72c42a304c2eb534c954b63f6246fc1594ba4157
Arthas217 <442854399@qq.com>
2022-04-16 13:10:21 +0800

    schedule go on

--- src/main/java/com/example/study01/Study01Application.java
+++ src/main/java/com/example/study01/Study01Application.java
@@ -3,8 +3,10 @@ package com.example.study01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableScheduling //开启定时任务
 public class Study01Application {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/study01/schedule/SpringbootScheduleTask.java
@@ -0,0 +1,19 @@
+package com.example.study01.schedule;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+@Configuration
+public class SpringbootScheduleTask {
+
+    @Scheduled(cron = "0/5 * * * * ?")  //每隔5s 添加定时任务
+    //@Scheduled(fixedRate=5000)        //或直接指定时间间隔，例如：5秒
+    private void configureTasks() {
+        System.err.println("执行静态定时任务时间: " + LocalDateTime.now());
+    }
+}
