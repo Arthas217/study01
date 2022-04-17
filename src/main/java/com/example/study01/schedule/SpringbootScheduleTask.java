08455c3a5963417378e07f08b40b4e4243a89d08
zuocheng <442854399@qq.com>
2022-04-17 09:48:44 +0800

    go on

--- src/main/java/com/example/study01/schedule/SpringbootScheduleTask.java
+++ src/main/java/com/example/study01/schedule/SpringbootScheduleTask.java
@@ -9,9 +9,11 @@ import java.time.LocalDateTime;
 @Component
 @Configuration
 public class SpringbootScheduleTask {
-
-    @Scheduled(cron = "0/5 * * * * ?")  //每隔5s 添加定时任务
-    //@Scheduled(fixedRate=5000)        //或直接指定时间间隔，例如：5秒
+    /**
+     * 每隔5s 添加定时任务
+     * 或直接指定时间间隔，例如：5秒 @Scheduled(fixedRate=5000)
+     */
+    @Scheduled(cron = "0/5 * * * * ?")
     private void configureTasks() {
         System.err.println("执行静态定时任务时间: " + LocalDateTime.now());
     }
